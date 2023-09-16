a5c7d7aeff03f327b861bb22b68bc83198ca8dce
Alireza-Khajehvandi <alireza76khajehvandi@gmail.com>
2023-09-16 22:47:25 +0330

    models and database had created
    - projects model have created
    - contact model have created

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from resume.models import Projects, Contact
 
+
+class ProjectsAdmin(admin.ModelAdmin):
+    date_hierarchy = "published_date"
+    empty_value_display = "-empty-"
+    # fields = ["title"]
+    # exclude = ["title"] # ignore this field
+    list_display = ["title", "counted_views", "status", "published_date", "created_date"]
+    list_filter = ["status"]
+    # ordering = ["created_date"]
+    serch_fields = ["title", "content"]
+
+admin.site.register(Projects, ProjectsAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ["name", "email", "created_date"]
+    list_filter = ["email"]
+    serch_fields = ["name", "message"]
+    
 # Register your models here.
+admin.site.register(Contact, ContactAdmin)
