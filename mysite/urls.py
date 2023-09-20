43523e04199366a22fed491a7426d75db2cce71f
Alireza-Khajehvandi <alireza76khajehvandi@gmail.com>
2023-09-20 19:54:16 +0330

    website_index.html file created
    - website_index.html created very simple with no header and footer, just with my full name

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static #new
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("resume.urls")), #new
+    path("website/", include("website.urls")), #new
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #new
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #new
--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+import datetime
+from resume.models import Project
+from django.db.models import Q
 
 # Create your views here.
+def website_index(request):
+    return render(request, "website/website_index.html")
