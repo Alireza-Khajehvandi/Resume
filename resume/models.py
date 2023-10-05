effff6a10046791065afca37b49ccce0dd8a1bd0
Alireza-Khajehvandi <alireza76khajehvandi@gmail.com>
2023-10-05 17:42:52 +0330

    Add projects and some categories

--- resume/models.py
+++ resume/models.py
@@ -28,7 +28,7 @@ class Project(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_date"]
+        ordering = ["created_date"]
 
     def __str__(self):
         return self.title
